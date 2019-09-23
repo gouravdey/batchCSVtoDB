d0d79ac7a31d748d81612fe48fd8afda31ef7a0c
gouravdey <dey.gourav@gmail.com>
2019-09-23 11:20:18 +0530

    Add files via upload

---
+++ batchCSVtoDB/BatchCsVtoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.batchCSVtoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchCsVtoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchCsVtoDbApplication.class, args);
+	}
+
+}
---
+++ resources/db.sql
@@ -0,0 +1,7 @@
+create table employee (
+    id integer not null,
+    name varchar(255) not null,
+    age integer,
+    sex varchar(255),
+    primary key(id)
+);
